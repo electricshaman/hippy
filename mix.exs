25e51f578fd4f7582e19399d8b4d99eba2ea4a72
Jeff Smith <jeff@sohlsmith.com>
2018-04-19 00:09:32 -0500

    Prepare for initial release on hex.pm.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Hippy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Hippy",
+      source_url: "https://github.com/electricshaman/hippy"
     ]
   end
 
@@ -19,7 +23,24 @@ defmodule Hippy.MixProject do
 
   defp deps do
     [
-      {:httpoison, "~> 1.0"}
+      {:httpoison, "~> 1.0"},
+      {:ex_doc, "~> 0.18", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Hippy is an Internet Printing Protocol (IPP) client implementation in Elixir for performing
+    distributed printing over HTTP. It can be used with CUPS or network printers supporting IPP.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Jeff Smith"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/electricshaman/hippy"}
     ]
   end
 end
