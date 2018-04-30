3d00a3e9503b2703cb416d5328f3849b96f0a956
Jeff Smith <jeff@sohlsmith.com>
2018-04-30 13:17:27 -0500

    Bump version to 0.2.0 for release.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hippy.MixProject do
   def project do
     [
       app: :hippy,
-      version: "0.2.0-dev",
+      version: "0.2.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
